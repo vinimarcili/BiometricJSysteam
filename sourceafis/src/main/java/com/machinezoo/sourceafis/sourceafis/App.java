b4d0e51b241d90a51f32c0f1660567959fc70194
Vinícius Marcili <vinipetter@gmail.com>
2017-10-17 22:28:06 -0200

    First commit

---
+++ sourceafis/src/main/java/com/machinezoo/sourceafis/sourceafis/App.java
@@ -0,0 +1,13 @@
+package com.machinezoo.sourceafis.sourceafis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
